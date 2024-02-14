abaffcd496428e0bc797e52843db05e9803e36f0
EL-MOUAFIKT <titouan.elmouafik@gmail.com>
2024-02-14 17:52:18 +0100

    lasttime

---
+++ parties/index.php
@@ -0,0 +1,57 @@
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>index.html</title>
+</head>
+<body>
+//bootstrap stylesheet and js files
+<link href="../assets/css/bootstrap.min.css" rel="stylesheet">
+<script src="../assets/js/bootstrap.min.js"></script>
+
+<h1>Projet Cinema</h1>
+
+<h1>LISTE DES FILMS</h1>
+
+</body>
+</html>
+<?php
+//connexion à la base de données
+require_once "../assets/config/db-config.php";
+//requête SQL
+$sql = "SELECT * FROM films";
+
+//exécution de la requête
+$requetePDO=$connexion->prepare($sql);
+$requetePDO->execute();
+//récupération des résultats
+$films = $requetePDO->fetchAll(PDO::FETCH_ASSOC);
+//affichage des résultats
+foreach ($films as $film) {
+
+}
+?>
+
+<?php
+foreach ($films as $film) {
+    $titre= $film['titre'];
+    $image= $film['image'];
+    $annee= $film['annee'];
+    $genre= $film['genre'];
+    $duree= $film['durée'];
+}
+?>
+<div class="row row-cols-1 row-cols-md-3 g-4">
+    <div class="col">
+        <div class="card">
+            <img src="<?php $image?> class="card-img-top" alt="...">
+            <div class="card-body">
+                <h5 class="card-title"><?php $titre?>/h5>
+                <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+            </div>
+        </div>
+    </div>
+</div>
+
+
