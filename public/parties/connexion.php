fe022b95452a3d3faf04c8640a1553965c38582c
EL-MOUAFIKT <titouan.elmouafik@gmail.com>
2024-03-27 17:53:27 +0100

    t

--- public/parties/connexion.php
+++ public/parties/connexion.php
@@ -1,30 +1,52 @@
-
 <?php
+session_start();
 //connexion à la base de données
 require_once "../../src/config/db-config.php";
 ?>
+
 <header><?php
 require_once "header.php"; ?></header>
+
 <?php
 require_once "../../src/databases/user.php";
-//déclaration des variables
-$email = '';
-$mdp = '';
-if ($_SERVER['REQUEST_METHOD'] === 'POST') {
-    //récupération des données
-    if (isset($_POST['mdp']) && isset($_POST['email'])) {
-        $mdp = $_POST['mdp'];
-        $email = $_POST['email'];
-        $mdp = password_hash($mdp, PASSWORD_DEFAULT);
-        //function login
-         if (login($email, $mdp)){
-            echo "vous êtes connecté";
-            header('Location: ../index.php');
-        }else{
-            echo "le mot de passe est incorrect";
-        }
-    }
+
+$logins=fetchAllUsers();
+//2. Récupérer les données
+$email='';
+$mdp='';
+//récupération des données du formulaire utilisateur
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $email = isset($_POST['email']) ? $_POST['email'] : null;
+    $mdp = isset($_POST['mdp']) ? $_POST['mdp'] : null;
 }
+//3. Valider les données:
+$erreurs = [];
+if (empty($email)){
+    $erreurs['email'] = "l'email est obligatoire";
+}
+if (empty($mdp)){
+    $erreurs['mdp'] = "le mot de passe est obligatoire";
+}
+
+// Comparer l'email formulaire avec l'email bdd pour pouvoir trouver le mdp bdd (relier a l'email bdd)
+// comparer l'email bdd avec l'email formulaire
+// comparer a l'aide de la fonction password_verify le mdp bdd avec le mdp formulaire
+
+if (empty($erreurs)){
+    $_SESSION=[
+        'email' => $email,
+        'mdp' => $mdp
+    ];
+    echo $_SESSION["email"];
+    echo $_SESSION["mdp"];
+    header('Location: ../index.php' );
+}
+//regarder si le tableau $user est vide et aller chercher les données de l'utilisateur
+//$user = userExiste();
+
+
+
+
 ?>
 <!DOCTYPE html>
 <html lang="en">
@@ -40,7 +62,20 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
 <link href="../assets/css/bootstrap.min.css" rel="stylesheet">
 <script src="../public/assets/js/bootstrap.min.js"></script>
 <div class="col-md-6 offset-md-3">
-<form method="POST">
+
+    <?php
+    if (isset($_SESSION['success'])) {
+        echo '<div class="alert alert-success">' . $_SESSION['success'] . '</div>';
+        unset($_SESSION['success']); // remove the message after displaying it
+    }
+
+    if (isset($_SESSION['error'])) {
+        echo '<div class="alert alert-danger">' . $_SESSION['error'] . '</div>';
+        unset($_SESSION['error']); // remove the message after displaying it
+    }
+    ?>
+
+    <form method="POST">
     <div class="mb-3">
         <label for="exampleemail" class="form-label">email</label>
         <input type="email"
@@ -53,7 +88,7 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     </div>
 <div class="mb-3">
     <label for="exampleInputPassword1" class="form-label">mdp</label>
-    <input type="mdp"
+    <input type="password"
            name="mdp"
            class="form-control"
            id="exampleInputPassword1"
--- public/parties/test.php
+++ public/parties/test.php
@@ -1,5 +1,10 @@
 <?php
-//page d'inscription
-//connexion à la base de données
-require_once "../assets/config/db-config.php";
-$email= $_POST['email'];
+$password = "Testeur46!";
+//$hash = "$2y$10$9AaYn8DqptSbXBGXELWOD.6ULHxUFfWCIJlLWExevEQhssq29Cp8i";
+$hash = password_hash($password, PASSWORD_DEFAULT);
+
+if (password_verify($password, $hash)) {
+    echo 'Password is valid!';
+} else {
+    echo 'Invalid password.';
+}
